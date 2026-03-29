81b1bff3f6488f26806a0da1301da57b6c4e5876
Jarold Ruda <jarold1984@gmail.com>
2026-03-28 21:10:42 -0500

    Add PWA support: manifest, service worker and icons

---
+++ public/sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'todo-v1';
+const urlsToCache = [
+  '/page-todo-list/',
+  '/page-todo-list/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+      .catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/page-todo-list/index.html');
+        }
+      })
+  );
+});
